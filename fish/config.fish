99cde133f675dc7904b5cad688ea86550a534c5f
frayans <frayans@protonmail.com>
2024-08-26 16:04:18 +0800

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+# abbreviations
+abbr --add hx --position command helix # hx -> helix
+abbr --add brctl --position command brightnessctl # brctl -> brightnessctl
+abbr --add sctl --position command systemctl # sctl -> systemctl
+
+# set starship as prompt
+starship init fish | source
